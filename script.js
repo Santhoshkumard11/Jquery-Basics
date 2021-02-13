8925cd41f52261a1cd10fc912ff416052e61badd
SANTHOSHKUMARD <31656855+Santhoshkumard11@users.noreply.github.com>
2021-02-13 14:05:37 +0530

    done T02-001 mod with event listner

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-function add_two_numbers(lst) {
-  let numb1 = $("#first_number").val();
+function add_two_numbers() {
+    let numb1 = $("#first_number").val();
   let numb2 = $("#second_number").val();
 
   if (numb1 == "" || numb2 == "") {
-    $("#title").text("Pls. Enter the Values").css({ color: "red" });
-  } else {
-    let result = parseInt(numb1) + parseInt(numb2);
-    $("#title").text(result).css({ color: "green" });
-    $("#answer").val(result);
-  }
+      $("#title").text("Pls. Enter the Values").css({ color: "red" });
+    } else {
+        let result = parseInt(numb1) + parseInt(numb2);
+        $("#title").text(result).css({ color: "green" });
+        $("#answer").val(result);
+    }
+}
+
+window.onload=function(){
+    document.getElementById("btnSubmit").addEventListener("click", function () {
+        add_two_numbers();
+    });
 }
